20e0cff1db58053f57963034b3553140f3feb76d
tkdpython <30746605+tkdpython@users.noreply.github.com>
2020-08-21 14:48:48 +0100

    Create pip-createrepo.py

---
+++ pip-createrepo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os, shutil
+
+pipdir = '/repo/local/pip'
+
+
+def move2dir(fname):
+    filename = fname.split("-")
+    libname = filename[0]
+    dirname = libname.replace("_","-").lower()
+
+    print(f"{fname} will be moved to {dirname}")
+    dirpath = pipdir + "/" + dirname
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+    try:
+        shutil.move(pipdir + "/" + fname, dirpath + "/" + fname)
+    except:
+        print(f"Failed to process {fname}")
+
+
+for fname in os.listdir(pipdir):
+    if fname.endswith(".whl") or fname.endswith(".tar.gz") or fname.endswith(".tgz"):
+        move2dir(fname)
